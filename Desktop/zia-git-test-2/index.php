955c1258c1ca242b341b044bc1140dd548ce7d71
soehardi <syukur.suhardi89@gmail.com>
2023-11-26 07:30:01 +0700

    menambahkan paragraph

--- Desktop/zia-git-test-2/index.php
+++ Desktop/zia-git-test-2/index.php
@@ -7,5 +7,7 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita eligendi consectetur enim doloribus odit, debitis eum? Esse animi, minus magni neque suscipit illo quas ea dolorum aliquid nemo aut pariatur.
+    Nobis eligendi doloremque molestiae earum voluptatibus dolore beatae voluptatem delectus quaerat blanditiis et velit quod doloribus officia rem voluptate repellat nulla provident, distinctio ipsa autem. Quibusdam delectus odit molestias earum?</p>
 </body>
 </html>
